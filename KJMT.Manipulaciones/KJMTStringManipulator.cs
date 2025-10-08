6d11e9f56467c26ed63eb7675d1f47426eb1e582
KevinJose15 <mt25001@esfe.agape.edu.sv>
2025-10-07 22:56:49 -0600

    Se creo la clase manipulacion y se corriguieron errores

---
+++ KJMT.Manipulaciones/KJMTStringManipulator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KJMT.Manipulaciones
+{
+    public class KJMTStringManipulator
+    {
+        public string ReverseString(string input)
+        {
+            // Implementa la inversión de la cadena
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            // Implementa la eliminación de espacios en blanco (solo ' ')
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            return input.Replace(" ", string.Empty);
+        }
+    }
+}
